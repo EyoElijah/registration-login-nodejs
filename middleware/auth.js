42b501008da04c59978a29fac07bb70eab1333fb
EYO@DESKTOP-8MSK2NT <eyoelijah@gmail.com>
2022-03-24 15:30:46 +0100

    initial commit

---
+++ middleware/auth.js
@@ -0,0 +1,13 @@
+export function checkNotAuthenticated(req, res, next) {
+	if (req.isAuthenticated()) {
+		return res.redirect('/');
+	}
+	next();
+}
+
+export function checkAuthenticated(req, res, next) {
+	if (req.isAuthenticated()) {
+		return next();
+	}
+	res.redirect('/login');
+}
